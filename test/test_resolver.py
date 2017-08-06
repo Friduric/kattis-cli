587431c5681ab3e7fdd4853f23015e73bc363d82
Henke Adolfsson <catears@catears.se>
2017-08-06 17:47:34 +0200

    Add failing test for two rules resolving to same goal

--- test/test_resolver.py
+++ test/test_resolver.py
@@ -11,5 +11,18 @@ def test_resolve_basic():
     result = resolver.resolve(ruleset, context)
     assert any(goal.name == 'resolve-basic-goal' and goal.points == 1
                for goal in result.goals)
+    assert len(result.goals) == 1
 
+
+def test_resolve_two_to_same():
+    rule_path = get_rule_file('test_resolve_two_to_same.json')
+    ruleset = rules.parse_file(rule_path.as_posix())
+    assert len(ruleset.rules) == 2
+
+    context = resolver.make_context()
+    result = resolver.resolve(ruleset, context)
+    assert len(result.goals) == 1
+    goal = result.goals[0]
+    assert goal.name == 'resolve-two-of-same' and goal.points == 2 and \
+        len(goal.resolved_rules) == 2 and len(goal.non_resolved_rules) == 0
 
