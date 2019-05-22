a5416bbfe51c8207ac0f7320fa13d3da27c3692c
Friduric <prantare@gmail.com>
2019-05-22 11:58:09 +0200

    Make teacher_main.sh default to non-detailed

--- scripts/teacher_main.sh
+++ scripts/teacher_main.sh
@@ -1,4 +1,4 @@
 source venv/bin/activate
 export PYTHONPATH=./src:./plugins
-python src/main.py --detailed --data data/AAPS-AAPS18_export_all.json --rules rules/rules.json $@
+python src/main.py --data data/AAPS-AAPS18_export_all.json --rules rules/rules.json $@
 
